10a4426866816e5845680c3affe8d18812739790
ZerrickStaples <zerrick.staples@gmail.com>
2018-01-10 23:05:20 -0500

    Wrote first failing test for write function

--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
 import Mocha from 'mocha';
 import { expect } from 'chai';
+import { write } from '../src/index';
 
 describe("Canary test", () => {
     it("True equals true", () => {
         expect(true).to.equal(true);
     });
 });
+
+describe("Pencil durability", () => {
+
+    describe("Write function", () => {
+        it("Write returns a string", () => {
+            expect(write()).to.equal("She sells sea shells");
+        })
+    })
+
+})
